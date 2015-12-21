8287fcec82a7eb1d60c9cd84dafba1131055daa8
BrOrlandi <brorlandi@gmail.com>
2015-12-21 16:33:30 -0200

    Automated Build with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var sass = require('gulp-sass');
- 
+var del = require('del');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+
 gulp.task('connect', function() {
   connect.server({
     root: 'public',
@@ -15,7 +20,7 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./public'))
     .pipe(connect.reload());
 });
- 
+
 
 gulp.task('reload', function () {
   gulp.src(['./public/**/*','!./public/**/*.scss'])
@@ -26,5 +31,20 @@ gulp.task('watch', function () {
   gulp.watch(['./public/**/*','!./public/**/*.scss'],['reload']);
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
- 
+
+gulp.task('clean-build',function(){
+    del.sync('./dist/*');
+});
+
+gulp.task('build',['sass','clean-build'],function(){
+    gulp.src('public/*.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+
+    gulp.src(['./public/*.*','!public/*.html','!public/*.css','!public/*.js'])
+        .pipe(gulp.dest('./dist'));
+});
+
  gulp.task('default', ['sass','connect','watch']);
