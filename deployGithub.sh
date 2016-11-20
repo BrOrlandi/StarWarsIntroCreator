fb52a623c32c2ed29436cebbeb731ac3cdfe56eb
BrOrlandi <brorlandi@gmail.com>
2016-11-20 17:58:48 -0200

    Add deploy script

---
+++ deployGithub.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    echo 'Use: ./deployGithub.sh "Commit message"'
+    exit 1
+fi
+
+git checkout master
+npm run build
+git checkout gh-pages
+mv dist/* ./
+mv dist/.* ./
+git add .
+git commit -m "$1"
+git push
+git checkout master
