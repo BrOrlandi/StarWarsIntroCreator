7e930dbb500d800a70336379a6fcf6c694c9da87
BrOrlandi <brorlandi@gmail.com>
2018-05-24 00:25:37 -0300

    Redirect users to new website.

---
+++ public/redirectToNewWebsite.js
@@ -0,0 +1,56 @@
+const stageNow = 1;
+const redirectPercentage = 0.2;
+const redirectActive = true;
+
+function saveRedirect(redirectInfo) {
+  localStorage.KasselLabsRedirect = JSON.stringify(redirectInfo);
+}
+
+function isFallback() {
+  return location.search.indexOf('fallback') > -1;
+}
+
+function redirectToNewWebsite(){
+  location.assign('https://starwarsintrocreator.kassellabs.io/?ref=redirect'+location.hash);
+}
+
+function randomShouldRedirect() {
+  return Math.random() < redirectPercentage;
+}
+
+$(document).ready(function() {
+  let redirectInfo =  localStorage.KasselLabsRedirect;
+  redirectInfo = redirectInfo && JSON.parse(redirectInfo);
+
+  checkRedirect(redirectInfo);
+});
+
+function checkRedirect(redirectInfo) {
+  if (!redirectInfo || (redirectInfo.stage < stageNow && !redirectInfo.redirect)) {
+    const shouldRedirect = randomShouldRedirect();
+    let redirect = false;
+    if (shouldRedirect) {
+      redirect = true;
+    }
+
+    redirectInfo = {
+      redirect: redirect,
+      stage: stageNow,
+    }
+  }
+
+  const fallback = isFallback();
+  if (fallback && redirectInfo.redirect) {
+    redirectInfo = {
+      redirect: false,
+      stage: stageNow,
+    }
+  }
+
+  saveRedirect(redirectInfo);
+
+  const redirect = redirectInfo.redirect;
+  if (redirect && redirectActive) {
+    redirectToNewWebsite();
+  }
+}
